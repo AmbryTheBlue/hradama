544537114e5b09b5ea1127375a71212afee45f17
AmbryTheBlue <61111243+AmbryTheBlue@users.noreply.github.com>
2020-07-23 12:02:31 +0200

    maly update

---
+++ script.js
@@ -0,0 +1,6 @@
+function generuj(){
+  var x = document.getElementById('sirka').value;
+  var y = document.getElementById('vyska').value;
+  var v = document.getElementById('vrstvy').value;
+  console.log(x,y,v);
+}
